3e89153fccf2e0b5f6ecf2f43cf18a24e678b1d0
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2020-07-02 21:11:47 -0300

    adicionando pagina de produtos de afiliacoes

---
+++ src/views/afiliacoes/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFF159",
+  },
+  contentContainerStyle: {
+    paddingBottom: 40,
+  },
+  card: {
+    width: "90%",
+    marginTop: 25,
+    alignSelf: "center",
+  },
+  imagem: {
+    alignSelf: "center",
+  },
+  titulos: {
+    fontWeight: "bold",
+  },
+});
