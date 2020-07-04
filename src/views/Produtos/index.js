e9d8953e37142e945a1ab4140de6702c5cf9c13d
felipedamasceno <felipedamasceno97@gmail.com>
2020-07-04 16:16:30 -0300

    produtos estilizado

--- src/views/Produtos/index.js
+++ src/views/Produtos/index.js
@@ -3,6 +3,8 @@ import { View, Text, ScrollView, Image } from "react-native";
 import { produtos } from "./produtos";
 import { styles } from "./styles";
 import share from "../../images/share.png";
+import StarRating from 'react-native-star-rating';
+
 import {
   Avatar,
   Card,
@@ -23,38 +25,51 @@ export default function Produto() {
       >
         {produtos.map((prod) => (
           <Card style={styles.card} key={prod.nome}>
+            <View style={styles.viewRow}>
             <Card.Cover
               source={{
                 uri: prod.imagem
               }} style={styles.cover}
             />
             <Card.Content>
-              <Title>{prod.nome}</Title>
+              <Title style={styles.titleSize}>{prod.nome}</Title>
               <Paragraph>
-                <Text style={styles.titulos}>Preço do Produto:</Text> R${" "}
-                {prod.valor}
+                <Text style={styles.titulos}>Preço: <Text style={{fontWeight: 'normal'}}>{prod.valor} R$</Text></Text> 
+                
               </Paragraph>
               <Paragraph>
-                <Text style={styles.titulos}>Valor da comissão:</Text> R${" "}
-                {prod.valorComissao}
+                <Text style={styles.titulos}>Comissão: <Text style={{fontWeight: 'normal'}}>{prod.valorComissao}% p/venda</Text>
+                </Text> 
+                
               </Paragraph>
               <Paragraph>
-                <Text style={styles.titulos}>Em estoque:</Text> {" "}
-                {prod.estoque}
+                <Text style={styles.titulos}>Em estoque: <Text style={{fontWeight: 'normal'}}>{prod.estoque}</Text></Text> 
+                
               </Paragraph>
               <Paragraph>
-                <Text style={styles.titulos}>Vendedor:</Text>{" "}
-                {prod.nomeVendedor}
+                <Text style={styles.titulos}>Vendedor: <Text style={{fontWeight: 'normal'}}>{prod.nomeVendedor}</Text></Text>
+
               </Paragraph>
-              <Paragraph>
-                <Text style={styles.titulos}>Rating:</Text> {prod.rating}
-              </Paragraph>
+              <View style={styles.viewRow}>
+              <StarRating
+                disabled={true}
+                maxStars={5}
+                rating={prod.rating}
+                fullStarColor={'#1D2C7C'}
+                starSize={25}
+              />
+              <Text style={{color: '#1D2C7C'}}>  {prod.rating}</Text>
+              </View>
             </Card.Content>
+            </View>
+            <View style={styles.viewRow}>
             <Card.Actions>
               <Button mode="contained">
-              Link de Afiliado
+              Copiar
               </Button>
             </Card.Actions>
+            <Text style={styles.link}> {prod.link} </Text>
+            </View>
           </Card>
         ))}
       </ScrollView>
--- src/views/Produtos/styles.js
+++ src/views/Produtos/styles.js
@@ -13,18 +13,40 @@ export const styles = StyleSheet.create({
     alignSelf: "center"
     
   },
+
+  viewRow: {
+    
+    flexDirection: 'row'
+   },
+
+   titleSize:{
+    fontSize: 18,
+    color: '#1D2C7C',
+    fontWeight: 'bold',
+
+  },
+
+  link:{
+    justifyContent: "center",
+    alignSelf: "center",
+    fontSize: 10,
+    color: '#1D2C7C'
+  },
+
   imagem: {
     width: 15,
     height: 15
   },
+
   cover: {
-    width: 150,
-    height: 150,
+    width: 110,
+    height: 110,
     justifyContent: "center",
     alignSelf: "center",
   },
   titulos: {
     fontWeight: "bold",
+    color: '#1D2C7C',
    
   },
 });
