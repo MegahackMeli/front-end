df87eb87a48752e7323289d18a9deb429c5792ab
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2020-07-04 18:08:14 -0300

    adicionando filtro de busca

--- src/views/Produtos/index.js
+++ src/views/Produtos/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, ScrollView, Image } from "react-native";
+import { TextInput } from "react-native-paper";
 import { produtos } from "./produtos";
 import { styles } from "./styles";
 import share from "../../images/share.png";
-import StarRating from 'react-native-star-rating';
+import StarRating from "react-native-star-rating";
 
 import {
   Avatar,
@@ -15,64 +16,92 @@ import {
   Portal,
 } from "react-native-paper";
 export default function Produto() {
+  const [busca, setBusca] = useState("");
+
+  function retornaProdutos() {
+    if (!busca) {
+      return produtos;
+    } else {
+      return produtos.filter((x) =>
+        x.nome.toLowerCase().includes(busca.toLowerCase())
+      );
+    }
+  }
 
   return (
-    <>
-      
+    <View style={styles.container}>
+      <TextInput
+        label="Buscar"
+        value={busca}
+        onChangeText={(text) => setBusca(text)}
+        style={styles.input}
+      />
       <ScrollView
-        style={styles.container}
+        style={styles.scrollView}
         contentContainerStyle={styles.contentContainerStyle}
       >
-        {produtos.map((prod) => (
+        {retornaProdutos().map((prod) => (
           <Card style={styles.card} key={prod.nome}>
             <View style={styles.viewRow}>
-            <Card.Cover
-              source={{
-                uri: prod.imagem
-              }} style={styles.cover}
-            />
-            <Card.Content>
-              <Title style={styles.titleSize}>{prod.nome}</Title>
-              <Paragraph>
-                <Text style={styles.titulos}>Preço: <Text style={{fontWeight: 'normal'}}>{prod.valor} R$</Text></Text> 
-                
-              </Paragraph>
-              <Paragraph>
-                <Text style={styles.titulos}>Comissão: <Text style={{fontWeight: 'normal'}}>{prod.valorComissao}% p/venda</Text>
-                </Text> 
-                
-              </Paragraph>
-              <Paragraph>
-                <Text style={styles.titulos}>Em estoque: <Text style={{fontWeight: 'normal'}}>{prod.estoque}</Text></Text> 
-                
-              </Paragraph>
-              <Paragraph>
-                <Text style={styles.titulos}>Vendedor: <Text style={{fontWeight: 'normal'}}>{prod.nomeVendedor}</Text></Text>
-
-              </Paragraph>
-              <View style={styles.viewRow}>
-              <StarRating
-                disabled={true}
-                maxStars={5}
-                rating={prod.rating}
-                fullStarColor={'#1D2C7C'}
-                starSize={25}
+              <Card.Cover
+                source={{
+                  uri: prod.imagem,
+                }}
+                style={styles.cover}
               />
-              <Text style={{color: '#1D2C7C'}}>  {prod.rating}</Text>
-              </View>
-            </Card.Content>
+              <Card.Content>
+                <Title style={styles.titleSize}>{prod.nome}</Title>
+                <Paragraph>
+                  <Text style={styles.titulos}>
+                    Preço:{" "}
+                    <Text style={{ fontWeight: "normal" }}>
+                      {prod.valor} R$
+                    </Text>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text style={styles.titulos}>
+                    Comissão:{" "}
+                    <Text style={{ fontWeight: "normal" }}>
+                      {prod.valorComissao}% p/venda
+                    </Text>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text style={styles.titulos}>
+                    Em estoque:{" "}
+                    <Text style={{ fontWeight: "normal" }}>{prod.estoque}</Text>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text style={styles.titulos}>
+                    Vendedor:{" "}
+                    <Text style={{ fontWeight: "normal" }}>
+                      {prod.nomeVendedor}
+                    </Text>
+                  </Text>
+                </Paragraph>
+                <View style={styles.viewRow}>
+                  <StarRating
+                    disabled={true}
+                    maxStars={5}
+                    rating={prod.rating}
+                    fullStarColor={"#1D2C7C"}
+                    starSize={25}
+                  />
+                  <Text style={{ color: "#1D2C7C" }}> {prod.rating}</Text>
+                </View>
+              </Card.Content>
             </View>
             <View style={styles.viewRow}>
-            <Card.Actions>
-              <Button mode="contained">
-              Copiar
-              </Button>
-            </Card.Actions>
-            <Text style={styles.link}> {prod.link} </Text>
+              <Card.Actions>
+                <Button mode="contained">Copiar</Button>
+              </Card.Actions>
+              <Text style={styles.link}> {prod.link} </Text>
             </View>
           </Card>
         ))}
       </ScrollView>
-    </>
+    </View>
   );
 }
--- src/views/Produtos/styles.js
+++ src/views/Produtos/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     backgroundColor: "#FFF159",
+    height: "100%",
   },
   contentContainerStyle: {
     paddingBottom: 40,
@@ -10,34 +11,26 @@ export const styles = StyleSheet.create({
   card: {
     width: "90%",
     marginTop: 25,
-    alignSelf: "center"
-    
+    alignSelf: "center",
   },
-
   viewRow: {
-    
-    flexDirection: 'row'
-   },
-
-   titleSize:{
-    fontSize: 18,
-    color: '#1D2C7C',
-    fontWeight: 'bold',
-
+    flexDirection: "row",
   },
-
-  link:{
+  titleSize: {
+    fontSize: 18,
+    color: "#1D2C7C",
+    fontWeight: "bold",
+  },
+  link: {
     justifyContent: "center",
     alignSelf: "center",
     fontSize: 10,
-    color: '#1D2C7C'
+    color: "#1D2C7C",
   },
-
   imagem: {
     width: 15,
-    height: 15
+    height: 15,
   },
-
   cover: {
     width: 110,
     height: 110,
@@ -46,7 +39,14 @@ export const styles = StyleSheet.create({
   },
   titulos: {
     fontWeight: "bold",
-    color: '#1D2C7C',
-   
+    color: "#1D2C7C",
+  },
+  input: {
+    width: "90%",
+    marginTop: 25,
+    alignSelf: "center",
+  },
+  scrollView: {
+    marginTop: 15,
   },
 });
