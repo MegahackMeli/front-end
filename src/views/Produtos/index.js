1d1812c97ea4724b63aa1497b45f8bc6b80efe57
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2020-07-05 17:44:22 -0300

    ajustando

--- src/views/Produtos/index.js
+++ src/views/Produtos/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Clipboard } from "react-native";
 import { TextInput } from "react-native-paper";
 import { styles } from "./styles";
 import share from "../../images/share.png";
@@ -30,81 +30,93 @@ export default function Produto() {
   }
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        label="Buscar"
-        value={busca}
-        onChangeText={(text) => setBusca(text)}
-        style={styles.input}
-      />
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.contentContainerStyle}
-      >
-        {retornaProdutos().map((prod) => (
-          <Card style={styles.card} key={prod.nome}>
-            <View style={styles.viewRow}>
-              <Card.Cover
-                source={{
-                  uri: prod.imagem,
-                }}
-                style={styles.cover}
-              />
-              <Card.Content>
-                <Title style={styles.titleSize}>{prod.nome}</Title>
-                <Paragraph>
-                  <Text style={styles.titulos}>
-                    Preço:{" "}
-                    <Text style={{ fontWeight: "normal" }}>
-                      {prod.valor} R$
+    <>
+      <View style={styles.container}>
+        <TextInput
+          label="Buscar"
+          value={busca}
+          onChangeText={(text) => setBusca(text)}
+          style={styles.input}
+        />
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.contentContainerStyle}
+        >
+          {retornaProdutos().map((prod) => (
+            <Card style={styles.card} key={prod.nome}>
+              <View style={styles.viewRow}>
+                <Card.Cover
+                  source={{
+                    uri: prod.imagem,
+                  }}
+                  style={styles.cover}
+                />
+                <Card.Content>
+                  <Title style={styles.titleSize}>{prod.nome}</Title>
+                  <Paragraph>
+                    <Text style={styles.titulos}>
+                      Preço:{" "}
+                      <Text style={{ fontWeight: "normal" }}>
+                        {prod.valor} R$
+                      </Text>
                     </Text>
-                  </Text>
-                </Paragraph>
-                <Paragraph>
-                  <Text style={styles.titulos}>
-                    Comissão:{" "}
-                    <Text style={{ fontWeight: "normal" }}>
-                      {prod.valorComissao}% p/venda
+                  </Paragraph>
+                  <Paragraph>
+                    <Text style={styles.titulos}>
+                      Comissão:{" "}
+                      <Text style={{ fontWeight: "normal" }}>
+                        {prod.valorComissao}% p/venda
+                      </Text>
                     </Text>
-                  </Text>
-                </Paragraph>
-                <Paragraph>
-                  <Text style={styles.titulos}>
-                    Em estoque:{" "}
-                    <Text style={{ fontWeight: "normal" }}>{prod.estoque}</Text>
-                  </Text>
-                </Paragraph>
-                <Paragraph>
-                  <Text style={styles.titulos}>
-                    Vendedor:{" "}
-                    <Text style={{ fontWeight: "normal" }}>
-                      {prod.nomeVendedor}
+                  </Paragraph>
+                  <Paragraph>
+                    <Text style={styles.titulos}>
+                      Em estoque:{" "}
+                      <Text style={{ fontWeight: "normal" }}>
+                        {prod.estoque}
+                      </Text>
                     </Text>
-                  </Text>
-                </Paragraph>
-                <View style={styles.viewRow}>
-                  <StarRating
-                    disabled={true}
-                    maxStars={5}
-                    rating={prod.rating}
-                    fullStarColor={"#1D2C7C"}
-                    starSize={25}
-                  />
-                  <Text style={{ color: "#1D2C7C" }}> {prod.rating}</Text>
-                </View>
-              </Card.Content>
-            </View>
-            <View style={styles.viewRow}>
-              <Card.Content>
-                <Button style={styles.buttonShare} mode="contained">
-                  <Icon name="share" size={15} />
-                </Button>
-              </Card.Content>
-              <Text style={styles.link}> {prod.link} </Text>
-            </View>
-          </Card>
-        ))}
-      </ScrollView>
-    </View>
+                  </Paragraph>
+                  <Paragraph>
+                    <Text style={styles.titulos}>
+                      Vendedor:{" "}
+                      <Text style={{ fontWeight: "normal" }}>
+                        {prod.nomeVendedor}
+                      </Text>
+                    </Text>
+                  </Paragraph>
+                  <View style={styles.viewRow}>
+                    <StarRating
+                      disabled={true}
+                      maxStars={5}
+                      rating={prod.rating}
+                      fullStarColor={"#1D2C7C"}
+                      starSize={25}
+                    />
+                    <Text style={{ color: "#1D2C7C" }}> {prod.rating}</Text>
+                  </View>
+                </Card.Content>
+              </View>
+              <View style={styles.viewRow}>
+                <Card.Content>
+                  <Button
+                    style={styles.buttonShare}
+                    mode="contained"
+                    onPress={() => {
+                      Clipboard.setString(
+                        "Hey, veja só esse produto do Meli: " + prod.link
+                      );
+                    }}
+                  >
+                    <Icon name="share" size={15} />
+                  </Button>
+                </Card.Content>
+                <Text style={styles.link}> {prod.link} </Text>
+              </View>
+            </Card>
+          ))}
+        </ScrollView>
+      </View>
+    </>
   );
 }
